2477b7194f74cf9368044add0f5942cb88aeac92
Emmanuel Kibicho <emmanuel.kibicho@outlook.com>
2020-04-01 12:34:00 -0400

    Got header file path

---
+++ robotRat.cpp
@@ -0,0 +1,13 @@
+#include <C:\Users\Admin\Desktop\c++\robotRat.h>
+#include <iostream>
+
+void displayMenu() {
+   cout << " 1. Pen Up\n";
+   cout << " 2. Pen down\n";
+   cout << " 3. Turn right\n";
+   cout << " 4. Turn left\n";
+   cout << " 5. Move\n";
+   cout << " 6. Print floor\n";
+   cout << " 7. Exit\n"; 
+
+}
