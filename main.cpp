3ba4eea15b27721f52c0f525b41e5d5ada167af9
Emmanuel Kibicho <emmanuel.kibicho@outlook.com>
2020-04-01 13:10:57 -0400

    for(;;) is infinite loop

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <C:\Users\Admin\Desktop\c++\robotRat.h>
+
+using namespace std;
+
+int main() {
+   for(;;){ //Infinite loop
+      displayMenu();
+      processMenuChoice();
+   }
+   return 0;
+}
